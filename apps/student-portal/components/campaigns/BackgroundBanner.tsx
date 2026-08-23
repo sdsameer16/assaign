9ac89c140b7a11992e67c51baecf70bd262ab396
sdsameer16 <sdsameerayaz2005@gmail.com>
2026-08-23 06:53:17 +0530

    refresh-error-for mobile

--- apps/student-portal/components/campaigns/BackgroundBanner.tsx
+++ apps/student-portal/components/campaigns/BackgroundBanner.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const BackgroundBanner: React.FC = () => {
   return (
-    <div className="absolute top-[64px] sm:top-[68px] md:top-[72px] left-0 right-0 w-full h-[450px] sm:h-[550px] md:h-[650px] pointer-events-none z-10 overflow-hidden">
+    <div className="absolute top-[140px] sm:top-[68px] md:top-[72px] left-0 right-0 w-full h-[450px] sm:h-[550px] md:h-[650px] pointer-events-none z-10 overflow-hidden">
       {/* Transparent Desktop Festive Overlay (Image 1 Background Removed) */}
       <img
         src="/rakhi/desktop-bg.png"
